377bdc75de2f53585af6eec1cc0f270ea2da5167
phlpmwa <mwaphiwa@gmail.com>
2020-03-17 06:53:54 -0700

    Data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el:"#vue-app",
     data:{
      name:'Philip',
-     job:'Ninja'
+     job:'Ninja',
+        website:'https://laravel.com/',
+        websiteTag:'<a href="https://laravel.com/">Laravel website</a>'
     },
     methods:{
  greet:function(time){
